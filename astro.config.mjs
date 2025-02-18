5cdf3ce7b110d4770b2c329a9c2bd1cd39d995da
Murat Polat Ozkan <muratpolatozkan@gmail.com>
2025-02-18 16:12:54 +0300

    fix(workflow): set NODE_ENV for production build and update base path in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import starlightImageZoom from 'starlight-image-zoom';
 export default defineConfig({
   srcDir: './astro',
   outDir: './astro/dist',
+  base: process.env.NODE_ENV === 'production' ? '/library' : '/',
   integrations: [
     lit(),
     starlight({
