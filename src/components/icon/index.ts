df6b7463854a3c849d4593bd8530f2e34dae2782
Murat Polat Ozkan <muratpolatozkan@gmail.com>
2025-03-07 02:29:59 +0300

    feat(icon): add SVG icon component with predefined icon set

--- src/components/icon/index.ts
+++ src/components/icon/index.ts
@@ -3,3 +3,9 @@ import { PlusIcon } from './icon.js';
 export type * from './icon.js';
 
 customElements.define('plus-icon', PlusIcon);
+
+import { PlusSvgIcon } from './svg-icon.js';
+export type * from './svg-icon.js';
+export * from './icons';
+
+customElements.define('plus-svg-icon', PlusSvgIcon);
