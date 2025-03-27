a947a67c69e8388d898cb69d623f5c4a0319ef92
Murat Polat Ozkan <muratpolatozkan@gmail.com>
2025-03-27 04:35:02 +0300

    feat(divider): add PlusDivider component with customizable styles and variants for visual separation

---
+++ src/components/divider/index.ts
@@ -0,0 +1,6 @@
+import { PlusDivider } from './divider.js';
+
+export type * from './divider';
+export { baseDividerStyle } from './divider.style';
+
+customElements.define('plus-divider', PlusDivider);
